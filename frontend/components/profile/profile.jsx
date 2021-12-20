7cb562f51bdd479dd25e5aa0e8f5099b3b6722a3
your name here <roelliga@yahoo.com>
2021-12-20 12:51:19 -0800

    profile events list

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -21,6 +21,57 @@ class Profile extends React.Component{
                 userRsvps.push(rsvp)
             }
         })
+        return (
+            <div className="profile-outer">
+              <div className="profile-username">
+                <p>Welcome {current_user.username}!!</p>
+              </div>
+              <div>
+                {userEventss.length === 0 ? (
+                    <div className="detail-no-event">
+                      <div className="no-details">
+                        <p className="p-no-details">
+                          You have not yet created any events
+                        </p>
+                      </div>
+                      <div className="link-create-profile">
+                        <Link to="/events/new" className="p-create-event">
+                          Create Event
+                        </Link>
+                      </div>
+                    </div>
+                ) : (
+                  <div className="yes-event-outer">
+                    <div className="your-event">
+                      <p className="p-your-event">Your Event</p>
+                    </div>
+                    {userEvents.map((event, index) => (
+                      <div key={event + "b"} className="user-event">
+                        <Link
+                          to={`/events/${event._id}`}
+                          className="link-profile"
+                        >
+                          <div className="profile-recip-title-image">
+                            {/* <div className="div-image-profile"> */}
+                              {/* <img
+                                src={listing.picture}
+                                alt="food-pic"
+                                className="images-profile"
+                              ></img>
+                            </div> */}
+                            <div className="event-title-profile">
+                              <p className="event-profile">{event.title}</p>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+      
+          );
     }
 }
 
